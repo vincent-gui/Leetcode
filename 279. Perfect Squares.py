a3cd914001e004d66a626efcc57b389e263aec6c
vincent-gui <vincent_gui@hotmail.com>
2021-05-03 23:00:49 -0700

    Update 279. Perfect Squares.py

--- 279. Perfect Squares.py
+++ 279. Perfect Squares.py
@@ -65,3 +65,14 @@ def numSquares(n):
 	for i in range(1, n+1):
 		dp[i] = min(dp[i-j*j] for j in range(1, int(i**0.5)+1)) + 1
 	return dp[n]
+
+
+
+FOLLOW UP
+
+有多少种方式把N 表示成完全平方数之和
+
+能不能把N 表示成恰好K 个完全平方数之和
+
+f[i][k] : i 能不能恰好分成k 段, 
+f[i][k] = or f[i - j**2][k - 1] 
