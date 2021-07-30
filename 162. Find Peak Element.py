474350fbabaaeac098e9d380333b667e247ea788
vincent-gui <vincent_gui@hotmail.com>
2021-07-30 10:41:55 -0700

    Create 162. Find Peak Element.py

---
+++ 162. Find Peak Element.py
@@ -0,0 +1,22 @@
+总结: 是一道mid 和 mid + 1, mid - 1 比较的题目
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+
+        
+        while start + 1 < end:
+            mid = (start + end) / 2
+            if nums[mid - 1] > nums[mid]:
+                end = mid
+            elif nums[mid + 1] > nums[mid]:
+                start = mid
+            else:
+                return mid
+            
+        if nums[start] > nums[end]:
+            return start
+        return end
