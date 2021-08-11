a4db70792212dc667fa1449847f8ab1aef01d45c
vincent-gui <vincent_gui@hotmail.com>
2021-08-10 22:11:48 -0700

    Update 378. Kth Smallest Element in a Sorted Matrix.py

--- 378. Kth Smallest Element in a Sorted Matrix.py
+++ 378. Kth Smallest Element in a Sorted Matrix.py
@@ -1,3 +1,49 @@
+解法: 二分答案, 用matrix 的左上角和右下角的值作为star 和end, 然后求出mid , 计算小于等于mid 的数是否超过了K 个
+
+当大于等于k 时, 让end = mid , 这一步必须这样, 非常重要, 这样可以找到第一个大于等于k 的mid , 并且这个mid 一定在matrix , 别问原因, 不在停不下来
+例: [[1,2],[2, 100]]
+
+
+
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        start, end = matrix[0][0], matrix[-1][-1]
+        
+        while start + 1 < end:
+            mid = (start + end) / 2
+
+            if self.count_less_or_equal(matrix, mid) < k:
+                start = mid
+            else:
+                end = mid
+        
+        if self.count_less_or_equal(matrix, start) >= k:
+            return start
+        return end
+        
+        
+        
+    def count_less_or_equal(self, matrix, num):
+        #从又上到左下
+        n, m = len(matrix), len(matrix[0])
+        i, j = 0, m - 1
+        cnt = 0
+        
+        while i < n and j >= 0:
+            if matrix[i][j] <= num:
+                cnt += j + 1
+                i += 1
+            else:
+                j -= 1
+        
+        return cnt
+
+
 
 解法: 因为从左到右, 从上到下, 单调递增, 所以从左上开始将数字加入minHeap, 向右和向下探索, 当循环k 次的时候, 就是第k 大的数
 
