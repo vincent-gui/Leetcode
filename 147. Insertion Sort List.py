f99eea6487918c39243c4195f898e8615f051c22
vincent-gui <vincent_gui@hotmail.com>
2021-12-16 21:54:53 -0800

    Update 147. Insertion Sort List.py

--- 147. Insertion Sort List.py
+++ 147. Insertion Sort List.py
@@ -1,6 +1,9 @@
 总结: 创建一个dummy node, 每次从head 开始搜索, 找到一个能够插入的节点, 和dummy 连接, 连接完再重新从dummy 开始扫描
 
 
+12/15/2021
+   #开一个新的头dummy， 往这个新的头里insert， 一个变量一直track 当前的curr， 另外一个每次从dummy 开始， 找到比curr。val 大的那个之前， 插入进去， 然后让curr 向前移动一位， prev 重制到dummy， 继续从头开始
+
 class Solution(object):
     def insertionSortList(self, head):
         """
