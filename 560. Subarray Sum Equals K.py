93eccdad4992079eeb6712f8d229168ca6b1cfb7
vincent-gui <vincent_gui@hotmail.com>
2022-02-09 23:27:37 -0800

    Update 560. Subarray Sum Equals K.py

--- 560. Subarray Sum Equals K.py
+++ 560. Subarray Sum Equals K.py
@@ -12,7 +12,7 @@ class Solution:
         cumSum = 0
         for num in nums:
             cumSum += num
-            ans += prefix.get(cumSum - k, 0)
+            ans += prefix.get(cumSum - k, 0) #为什么是先计算, 再更新呢, 因为要处理k= 0, [-1, 1], 第一轮cum 就是-1, 如果先更新, 那么-1 是出现了一次, 答案就会 +1, 但是这个并不是正确的
             prefix[cumSum] = prefix.get(cumSum, 0) + 1
         
         return ans
