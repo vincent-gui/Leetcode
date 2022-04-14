9372edeaa3e2f9c4d84a9a73141476ea48fb3c17
vincent-gui <vincent_gui@hotmail.com>
2022-04-13 23:05:49 -0700

    04/13/2022

--- 1091. Shortest Path in Binary Matrix.py
+++ 1091. Shortest Path in Binary Matrix.py
@@ -1,4 +1,21 @@
+题目:
+	从一个matrix 的左上角走到右下角的最短距离, 可以朝八个方向走, 只能走0, 不能走1
+	
+解法:
+	需要判断是否grid 为空, 或者grid[0] 是否为空
+	还需要判断是否[0][0] == 1, 如果是, 返回-1
+	
+	剩下标准bfs
+	
+	
+注意:
+However, like all in-place algorithms, overwriting the input can cause problems. Here are a couple of possible scenarios you need to consider.
 
+1. That the algorithm is running in a * multithreaded* environment, and it does not have exclusive access to the grid. Other threads might need to read the grid too, and might not expect it to be modified.
+
+2. That there is only a single thread or the algorithm has exclusive access to the grid while running, but the grid might need to be reused later or by another thread once the lock has been released.
+
+For the second scenario, Approach 1 could be modified to restore the grid—simply loop over it at the end, replacing all numbers that are greater than 1 with a 0, and additionally set the top-left cell to a 0.
 
 class Solution(object):
     def shortestPathBinaryMatrix(self, grid):
---
+++ 708. Insert into a Sorted Circular Linked List.py
@@ -0,0 +1,51 @@
+题目: 给了一个循环的递增链表, 和一个node , 插入这个node 并且保持链表递增
+
+一开始脑子就晕了, 在考虑怎么判定只有一个node, 因为不知道给的入口的大小, 所以必须需要两个node 才能搞清楚是在链表的什么阶段
+
+又考虑到如果只有一个node, 怎么返回
+
+还有就是考虑循环linkedlist 的循环退出条件
+while prev.next != head: 也就意味着只需要循环一次
+
+
+看了答案以后觉得还是答案厉害
+
+思路:
+	一共分成三种情况
+	1. 1 < 3 < 5, 3 是需要插入的结点
+	2. 5 -> 2 -> 4, 1或者6 是需要插入的结点, 整个时候需要找到 prev.val > curr.val 
+	3. 5-> 5-> 5, 2 是需要被插入的结点, 其实任何一个结点插入就可以, 可以合并到第一种情况
+	
+	
+	所以变成两种情况
+	1. 1 <= 3 <= 5, 3 是需要插入的结点
+	2. 5 -> 2 -> 4, 1或者6 是需要插入的结点, 整个时候需要找到 prev.val > curr.val 
+	
+解法如下
+
+class Solution:
+    def insert(self, head: 'Node', insertVal: int) -> 'Node':
+        node = Node(insertVal)  
+        
+        if not head:
+            node.next = node
+            return node
+
+        prev, curr = head, head.next
+        
+        while prev.next != head: #这个退出条件简直就是精华
+            # Case1: 1 <- Node(2) <- 3
+            if prev.val <= node.val <= curr.val:
+                break
+            
+            # Case2: 3 -> 1, 3 -> Node(4) -> 1, 3 -> Node(0) -> 1
+            if prev.val > curr.val and (node.val > prev.val or node.val < curr.val):
+                break
+            
+            prev, curr = prev.next, curr.next
+
+        # Insert node.
+        node.next = curr
+        prev.next = node           
+        
+        return head
