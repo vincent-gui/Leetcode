ffc0d5b7e1cf9e672d20cc89113a8fd428addcd6
vincent-gui <vincent_gui@hotmail.com>
2022-07-13 16:31:19 -0700

    Update 2158. Amount of New Area Painted Each Day.py

--- 2158. Amount of New Area Painted Each Day.py
+++ 2158. Amount of New Area Painted Each Day.py
@@ -48,3 +48,41 @@ class Solution:
         return ans
 paint = [[1,4],[4,7],[5,8]]
 Solution().amountPainted(paint)
+
+
+
+
+解法2: 维护了一个sorted containers
+思路就是对于每一个开始结束, 用扫描线
+对于每一个start或者end, 都维持一个(start/end, idx, flag)
+
+然后当如果一个pos 有多个idx 的时候
+比如 [[1,4],[4,7],[5,8]]
+	 [  0,     1,   2  ]
+pos 4 这个点有两个[(4, 0, -1), (4, 1, 1)] 这个里也维持的有序的
+如果flag== 1, 那么idx 加入sorted set, 如果-1 , 就移除, 每次sorted [0] 号为+= 下一个的pos
+
+
+from sortedcontainers import SortedDict, SortedSet
+class Solution:
+    def amountPainted(self, paint: List[List[int]]) -> List[int]:
+        records = SortedDict()
+        
+        for i, (start, end) in enumerate(paint):
+            records[start] = records.get(start, []) + [(i, 1)]
+            records[end] = records.get(end, []) + [(i, -1)]
+        s = SortedSet()
+        ans = [0] * len(paint)
+        array = [ (k, records[k]) for k in records.keys()]
+        for i in range(len(array)):
+            pos = array[i][0]
+            for idx, flag in array[i][1]:
+                if flag == 1:
+                    s.add(idx)
+                else:
+                    s.remove(idx)
+            
+            if i + 1< len(array) and s:
+                ans[s[0]] += array[i + 1][0] - pos
+                
+        return ans
